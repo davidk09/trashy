dc932b4c0cb1799473ff7686d6ed474ca728f5e2
Francisco Kusch <francisco.kusch@gmail.com>
2022-11-19 00:14:08 +0100

    Fixed dependencies and added MariaDB

--- webserver/src/main/java/com/example/trashy/controllers/OrderController.java
+++ webserver/src/main/java/com/example/trashy/controllers/OrderController.java
@@ -9,8 +9,4 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
 
 
-
-
-
-
 }
---
+++ webserver/src/main/java/com/example/trashy/services/OrderService.java
@@ -0,0 +1,4 @@
+package com.example.trashy.services;
+
+public class OrderService {
+}
