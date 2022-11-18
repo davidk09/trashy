a4839bc16527c249cb7c97e3f7e1439e8939aa05
David <dakratz@gmx.de>
2022-11-18 21:16:46 +0100

    structure / controller

---
+++ webserver/src/main/java/com/example/trashy/controllers/OrderController.java
@@ -0,0 +1,10 @@
+package com.example.trashy.controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("orders")
+public class OrderController {
+}
