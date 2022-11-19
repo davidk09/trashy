a9fd1db6e48408d345098781813d5086ed066417
ge95gaw <adri.l@live.de>
2022-11-19 17:00:50 +0100

    added sell and buy order popup

--- webclient/src/views/MarketplaceView.vue
+++ webclient/src/views/MarketplaceView.vue
@@ -6,35 +6,39 @@
       icon="arrow-left"
     />
     <div class="gapped-group">
-      <button class="button">Buy Trashcans</button>
-      <button class="button">Sell Trashcans</button>
+      <button class="button" @click="popup?.open(true)">Buy Trashcans</button>
+      <button class="button" @click="popup?.open(false)">Sell Trashcans</button>
     </div>
-    <h2 style="margin-top: 1rem">Your listings:</h2>
+    <h2 style="margin-top: 3rem">Your listings:</h2>
     <div>
-      <listing-component price="10" :buy="true" qty="100" />
-      <listing-component price="100" :buy="false" qty="10" />
+      <listing-component :price="10" :buy="true" :qty="100" />
+      <listing-component :price="100" :buy="false" :qty="10" />
     </div>
     <h2 style="margin-top: 1rem">Past transactions:</h2>
     <div>
       <transaction-component
-        price="100"
+        :price="100"
         :buy="false"
-        qty="10"
+        :qty="10"
         date="19.11.2022"
       />
       <transaction-component
-        price="10"
+        :price="10"
         :buy="true"
-        qty="100"
+        :qty="100"
         date="11.09.2022"
       />
     </div>
   </div>
+  <order-popup ref="popup" />
 </template>
 
 <script setup lang="ts">
 import PageTitle from "@/components/PageTitle";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import ListingComponent from "@/components/ListingComponent.vue";
 import TransactionComponent from "@/components/TransactionComponent.vue";
+import OrderPopup from "@/components/OrderPopup.vue";
+import { ref } from "vue";
+
+const popup = ref<typeof OrderPopup>();
 </script>
